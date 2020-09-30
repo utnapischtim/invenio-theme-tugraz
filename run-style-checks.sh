9e472a3bc282e2532d1eee7841cc6d1b611796de
Christoph Ladurner <christoph.ladurner@tugraz.at>
2020-09-30 10:47:40 +0200

    run-tests split testing and styling checks
    the styling checks are now in a separated file. this has been done to run the
    new run-style-checks.sh file as a pre-commit hook
    
    the pre-commit could look as the following snipped:
    
    report=$(sh run-style-checks.sh 2>&1)
    
    if [ ${#report} -gt 0 ]
    then
      echo "$report"
      exit 1
    else
      exit 0
    fi

--- run-style-checks.sh
+++ run-style-checks.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020 mojib wali.
+# Copyright (C) 2020 Graz University of Technology
 #
 # invenio-theme-tugraz is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
 
-pytest
+pydocstyle **/*.py
+isort **/*.py --quiet --check-only --diff
+check-manifest --quiet --ignore ".travis-*"
+sphinx-build -qnNW docs docs/_build/html
